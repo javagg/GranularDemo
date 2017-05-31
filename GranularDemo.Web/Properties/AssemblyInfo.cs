374bd84f11e1da83dcd3f1cb9eacca65afffcf29
lilu <lilu@miteke.com>
2017-05-31 23:54:20 +0800

    Add project files.

---
+++ GranularDemo.Web/Properties/AssemblyInfo.cs
@@ -0,0 +1,3 @@
+﻿[assembly: Bridge.Reflectable(Bridge.MemberAccessibility.ProtectedInstanceProperty)]
+[assembly: Granular.Compatibility.AssemblyReference(typeof(Granular.Host.WebApplicationHost))]
+[assembly: Granular.Compatibility.AssemblyReference(typeof(Granular.Presentation.Generic.ButtonChrome))]
